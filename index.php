27ba134a99f8883855621e5420a71150affaf637
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-04-13 13:39:01 +0300

    Functions and Filters

--- index.php
+++ index.php
@@ -21,73 +21,86 @@
         <h1>Recomended Books</h1>
 
         <?php
-            $books = [
-                [
-                    "name" => "The Knight of the Swords", 
-                    "author" => "Michael Moorcock", 
-                    "releaseYear" => 1971,
-                    "purchaseUrl" => 'http://example.com',
-                ],
-                [
-                    "name" => "The Queen of the Swords", 
-                    "author" => "Michael Moorcock", 
-                    "releaseYear" => 1971,
-                    "purchaseUrl" => 'http://example.com',
-                ],
-                [
-                    "name" => "The King of the Swords", 
-                    "author" => "Michael Moorcock", 
-                    "releaseYear" => 1971,
-                    "purchaseUrl" => 'http://example.com',
-                ],
-                [
-                    "name" => "The Time Machine", 
-                    "author" => "H. G. Wells", 
-                    "releaseYear" => 1895,
-                    "purchaseUrl" => 'http://example.com',
-                ],
-                [
-                    "name" => "The Invisible Man", 
-                    "author" => "H. G. Wells", 
-                    "releaseYear" => 1897,
-                    "purchaseUrl" => 'http://example.com',
-                ],
-                [
-                    "name" => "The War of the Worlds", 
-                    "author" => "H. G. Wells", 
-                    "releaseYear" => 1898,
-                    "purchaseUrl" => 'http://example.com',
-                ],
-                [
-                    "name" => "The Shining", 
-                    "author" => "Stephen King", 
-                    "releaseYear" => 1977,
-                    "purchaseUrl" => 'http://example.com',
-                ],
-                [
-                    "name" => "Pet Sematary", 
-                    "author" => "Stephen King", 
-                    "releaseYear" => 1983,
-                    "purchaseUrl" => 'http://example.com',
-                ],
-                [
-                    "name" => "The Green Mile", 
-                    "author" => "Stephen King", 
-                    "releaseYear" => 1996,
-                    "purchaseUrl" => 'http://example.com',
-                ]
-            ];
+        $books = [
+            [
+                "name" => "The Knight of the Swords",
+                "author" => "Michael Moorcock",
+                "releaseYear" => 1971,
+                "purchaseUrl" => 'http://example.com',
+            ],
+            [
+                "name" => "The Queen of the Swords",
+                "author" => "Michael Moorcock",
+                "releaseYear" => 1971,
+                "purchaseUrl" => 'http://example.com',
+            ],
+            [
+                "name" => "The King of the Swords",
+                "author" => "Michael Moorcock",
+                "releaseYear" => 1971,
+                "purchaseUrl" => 'http://example.com',
+            ],
+            [
+                "name" => "The Time Machine",
+                "author" => "H. G. Wells",
+                "releaseYear" => 1895,
+                "purchaseUrl" => 'http://example.com',
+            ],
+            [
+                "name" => "The Invisible Man",
+                "author" => "H. G. Wells",
+                "releaseYear" => 1897,
+                "purchaseUrl" => 'http://example.com',
+            ],
+            [
+                "name" => "The War of the Worlds",
+                "author" => "H. G. Wells",
+                "releaseYear" => 1898,
+                "purchaseUrl" => 'http://example.com',
+            ],
+            [
+                "name" => "The Shining",
+                "author" => "Stephen King",
+                "releaseYear" => 1977,
+                "purchaseUrl" => 'http://example.com',
+            ],
+            [
+                "name" => "Pet Sematary",
+                "author" => "Stephen King",
+                "releaseYear" => 1983,
+                "purchaseUrl" => 'http://example.com',
+            ],
+            [
+                "name" => "The Green Mile",
+                "author" => "Stephen King",
+                "releaseYear" => 1996,
+                "purchaseUrl" => 'http://example.com',
+            ]
+        ];
+
+        function filterByAuthor(array $books, string $author): array
+        {
+            $filteredBooks = [];
+
+            foreach ($books as $book) {
+                if ($book['author'] === $author) {
+                    $filteredBooks[] = $book;
+                }
+            }
+
+            return $filteredBooks;
+        }
         ?>
 
         <ul>
-            <?php foreach ($books as $book): ?>
+            <?php foreach (filterByAuthor($books, 'H. G. Wells') as $book): ?>
                 <li>
                     <a href="<?= $book['purchaseUrl']; ?>">
-                        <?= $book['name']; ?> (<?= $book['releaseYear']; ?>)
+                        <?= $book['name']; ?> (<?= $book['releaseYear']; ?>) - By <?= $book['author']; ?>
                     </a>
                 </li>
             <?php endforeach; ?>
         </ul>
-    </body>
 
+    </body>
 </html>
