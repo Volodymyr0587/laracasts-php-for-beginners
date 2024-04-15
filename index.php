f85dc57e1524fbdd42adef4fbae47d5a0f65d57e
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-04-15 11:52:14 +0300

    Render the Notes and Note Pages

--- index.php
+++ index.php
@@ -2,15 +2,4 @@
 
 require "functions.php";
 require "Database.php";
-// require "router.php";
-
-$config = require "config.php";
-
-$db = new Database($config['database']);
-
-$id = $_GET['id'];
-$query = "select * from notes where id = :id";
-
-$notes = $db->query($query, [':id' => $id])->fetch();
-
-dd($notes);
+require "router.php";
