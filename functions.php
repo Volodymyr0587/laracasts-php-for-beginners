8c640d4d5a9054bdd95dd78500ea1e8d4426cbb6
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-04-15 22:10:17 +0300

    Refactoring

--- functions.php
+++ functions.php
@@ -21,3 +21,10 @@ function urlIs($value)
 {
     return $_SERVER['REQUEST_URI'] === $value;
 }
+
+function authorize($condition, $status = Response::FORBIDDEN)
+{
+    if (! $condition) {
+        abort($status);   
+    }
+}
